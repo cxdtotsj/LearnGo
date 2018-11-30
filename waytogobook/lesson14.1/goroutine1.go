0c598f9c43b40755307328c311e98e4f41b735fb
bearchen <cxdtotsj@163.com>
2018-11-30 09:19:12 +0800

    test

--- waytogobook/lesson14.1/goroutine1.go
+++ waytogobook/lesson14.1/goroutine1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -10,4 +11,10 @@ func main() {
 
 func longwait() {
 	fmt.Println("Beginning longwait()")
+	time.Sleep(5 * 1e9)
+	fmt.Println("End of lognwait()")
+}
+
+func shortwait() {
+	fmt.Println("Beginning shortwait()")
 }
